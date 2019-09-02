b5f9863c37b6acc64e6dd9d420746794442716f3
aradosz79 <artur@icole.eu>
2019-09-02 18:04:12 +0200

    separated plugin from Portainer API

--- src/main/java/eu/icole/maven/portainer/DeploymentMojo.java
+++ src/main/java/eu/icole/maven/portainer/DeploymentMojo.java
@@ -1,12 +1,11 @@
 package eu.icole.maven.portainer;
 
+import eu.icole.portainer.PortainerConnection;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-
-import javax.sound.sampled.Port;
 
 @Mojo( name = "deploy")
 public class DeploymentMojo extends AbstractMojo {
